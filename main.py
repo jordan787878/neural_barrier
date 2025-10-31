cd9824a4d51ce8c822626160aa7f35725d70c363
Chun-Wei Kong <j2855001@gmail.com>
2025-10-31 16:27:45 -0600

    Add construction of GV directly from V so that CROWN can be used to compute bounds on GV

--- main.py
+++ main.py
@@ -355,6 +355,156 @@ def train_barrier_ibp(const, net, iterations=20000):
     return best["best_model"]
 
 
+def train_barrier_ibp_test(const, net, iterations=20000):
+    optimizer = torch.optim.Adam(net.parameters(), lr=1e-3)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1000, gamma=0.95)
+    batch_size = 500
+
+    # --- deterministic grid ---
+    _, _, grid_points_tensor = const.generate_grid(N=100)
+
+    inside_init = const.filter_sample_insidebound(grid_points_tensor, const.X_INIT_RANGE)
+    if inside_init.any():
+        grid_points_init = grid_points_tensor[inside_init]
+
+    inside_unsafe = const.filter_sample_insidebound(grid_points_tensor, const.X_UNSAFE_RANGE)
+    if inside_unsafe.any():
+        grid_points_unsafe = grid_points_tensor[inside_unsafe]
+
+    inside_goal = const.filter_sample_insidebound(grid_points_tensor, const.X_GOAL_RANGE)
+    if inside_goal.any():
+        grid_points_goal = grid_points_tensor[inside_goal]             
+
+    # --- training ---
+    best = {"best_iter":None, "best_model": None, "best_loss": np.inf}
+    for iter in range(iterations):
+        loss = torch.tensor(0.0)
+        optimizer.zero_grad()
+        vio_dict = {"init": None,
+                    "goal": None,
+                    "unsafe": None,
+                    "differential": None}
+
+        # --- loss init ---
+        x = const.sample_x(const.X_INIT_RANGE, batch_size)
+        x = torch.cat((x, grid_points_init), dim=0)
+        V = net(x)
+        violate = (const.ALPHA_RA < V)
+        violate_percent = 100.0*(violate.sum()/V.shape[0])
+        vio_dict["init"] = violate_percent.item()
+        loss_init = torch.mean(torch.relu(V - const.ALPHA_RA))
+        loss = loss + loss_init
+
+        # --- loss unsafe ---
+        x = const.sample_x(const.X_UNSAFE_RANGE, batch_size)
+        x = torch.cat((x, grid_points_unsafe), dim=0)
+        V = net(x)
+        violate = (V < const.BETA_RA)
+        violate_percent = 100.0*(violate.sum()/V.shape[0])
+        vio_dict["unsafe"] = violate_percent.item()
+        loss_unsafe =  torch.mean(torch.relu(const.BETA_RA - V))
+        loss = loss + loss_unsafe
+
+        # --- loss goal ---
+        """
+        change the loss_goal via:
+        the set Phi = {x such that V(x) <= 0.9} is 
+        (1) Phi is non-empty and
+        (2) Phi is a proper subset inside const.X_GOAL_RANGE
+        """
+        # 1) sample a batch from the full domain
+        x_full = const.sample_x(const.X_RANGE, batch_size)
+        x_full = torch.cat((x_full, grid_points_tensor), dim=0)
+        inside_goal = const.filter_sample_insidebound(x_full, const.X_GOAL_RANGE)
+        outside_goal = ~inside_goal
+        x = x_full[outside_goal]
+        V = net(x)
+        vio_dict["[info] V(not in x_goal) min"] = V.min().item()
+        violate = (V <= const.BETA_S)
+        violate_percent = 100.0*(violate.sum()/V.shape[0])
+        vio_dict["goal"] = violate_percent.item()
+        loss_goal_out = torch.mean(torch.relu(const.BETA_S - V))
+        loss = loss + loss_goal_out
+
+        x = const.sample_x(const.X_GOAL_RANGE, batch_size)
+        x = torch.cat((x, grid_points_goal), dim=0)
+        V = net(x)
+        vio_dict["[info] V(x_goal) min"] = V.min().item()
+        # hinge‐loss to keep 0 ≤ V_min ≤ const.BETA_S
+        loss_lower = torch.mean(torch.relu(0.0 - V.min()))             # penalizes V < 0
+        loss_upper = torch.mean(torch.relu(V.min() - const.BETA_S))    # penalizes V > const.BETA_S
+        loss_goal = loss_lower + loss_upper
+        loss = loss + loss_goal
+
+        # --- loss Differential ---
+        x = const.sample_x(const.X_RANGE, batch_size, requires_grad=True)
+        x = torch.cat((x, grid_points_tensor), dim=0)
+        inside_goal = const.filter_sample_insidebound(x, const.X_GOAL_RANGE)
+        if inside_goal.any():
+            x = x[~inside_goal]
+        V = net(x)
+        mask = (V.view(-1) <= const.BETA_RA)
+        if(mask.any()):
+            x = x[mask]
+        else:
+            raise("cannot find samples for differential constraint")
+        GV = diff_operator(const, net, x)
+        violate = (-const.ZETA < GV)
+        violate_percent = 100.0*(violate.sum()/GV.shape[0])
+        vio_dict["differential"] = violate_percent.item()
+        vio_dict["[info] GV max"] = GV.max()
+        loss_differential = torch.mean(torch.relu(GV + const.ZETA))
+        loss = loss + loss_differential
+
+        # --- loss Nonegative
+        # x = const.sample_x(const.X_RANGE, batch_size)
+        # x = torch.cat((x, grid_points_tensor), dim=0)
+        # V = net(x)
+        # violate = (V < 0.0)
+        # violate_percent = 100.0*(violate.sum()/V.shape[0])
+        # vio_dict["non-neg"] = violate_percent.item()
+        # loss_nonneg = torch.relu(0.0 - V).mean()
+        # loss = loss + loss_nonneg
+        
+        # --- optimize ---
+        if(loss.item() < 0.95*best["best_loss"]):
+            best["best_iter"] = iter
+            best["best_loss"] = loss.item()
+            best["best_model"] = net
+            print("[Best] iter {:3d}, loss: {:.4f}".format(iter, loss.item()))
+            for k, v in vio_dict.items():
+                if v is None:
+                    print(f".  {k}: None")
+                else:
+                    print(f".  {k}: {v:.4f}")
+        # if(loss.item() <= 0.0):
+        if(vio_dict["init"] <= 0
+           and vio_dict["unsafe"] <=0 
+           and vio_dict["goal"] <= 0
+           and vio_dict["[info] V(x_goal) min"] < const.BETA_S
+           and vio_dict["[info] V(x_goal) min"] > 0.0
+           and vio_dict["GV max"] < 0.0
+           ):
+            print("[Training Complete] no violation found")
+            best["best_iter"] = iter
+            best["best_loss"] = loss.item()
+            best["best_model"] = net
+            print("[Best] iter {:3d}, loss: {:.4f}".format(iter, loss.item()))
+            for k, v in vio_dict.items():
+                if v is None:
+                    print(f".  {k}: None")
+                else:
+                    print(f".  {k}: {v:.4f}")
+            return best["best_model"]
+        
+        loss.backward(retain_graph=True)
+        optimizer.step()
+        scheduler.step()
+
+    print("[training] exceed max ieteraions")
+    return best["best_model"]
+
+
 def visual_barrier(const, net):
     # --- build grid and evaluate V ---
     X, Y, grid_pts = const.generate_grid()               # X,Y: (M,M), grid_pts: (M*M,2)
@@ -521,7 +671,7 @@ def main():
         np.random.seed(0)
         net.apply(init_weights_xavier)
         # visual_barrier(const, net)
-        net = train_barrier_ibp(const, net, iterations=50000)
+        net = train_barrier_ibp_test(const, net, iterations=50000)
         if(net_path is not None):
             torch.save({
                         'model_state_dict': net.state_dict(),
@@ -533,15 +683,15 @@ def main():
         net = load_trained_model(net, net_path)
 
     # --- Post-process ---
-    check_barrier(const, net)
+    # check_barrier(const, net)
     # visual_barrier(const, net)
     # visual_barrier_3d(const, net)
 
     # --- Verification ---
     max_depth = 20
     # verify_ibp_differential(const, net, gif_path="figs/verification_ibp.gif", max_depth=max_depth)
-    verify_crown_differential(const, net, gif_path="figs/verification_crown.gif", max_depth=max_depth)
-    # verify_crown_differential_direct(const, net, gif_path="figs/verification_crown_direct.gif", max_depth=max_depth)
+    # verify_crown_differential(const, net, gif_path="figs/verification_crown.gif", max_depth=max_depth)
+    verify_crown_differential_direct(const, net, gif_path="figs/verification_crown_direct.gif", max_depth=max_depth)
 
 
 if __name__ == "__main__":
